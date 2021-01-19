4bc87ecc593f7acd34b29c4c7ca4b5369f6d7ced
ZenCarriere <59712949+ZenCarriere@users.noreply.github.com>
2021-01-19 14:34:11 -0500

    starting up

--- app.js
+++ app.js
@@ -1,59 +1,23 @@
-const http = require('http');//like a php require
-const stream = require('fs');
+const express = require('express');//like a php require
+const server = express();
+const path = require('path');
 
-//require is more or less the same as js import
+server.set("views", path.join(__dirname, 'views'));
+//set the static assets directory so express knows where to look
+//for css files, js files, images, etc - anything static
+server.use(express.static(path.join(__dirname, 'public')));
 
-// const hostname = '127.0.0.1';//this is localhost
+server.get("/", (req, res) => {
+  console.log('hit the home route');
+  res.sendFile('ko no dio da!')
+})
+
+server.get("/contact", (req, res) => {
+  console.log('hit the contact route');
+  res.send('contact page');
+} )
+
 const port = process.env.PORT || 3000;//localhost:3000 in browser to view
-
-const server = http.createServer((req, res) => {
-  res.statusCode = 200;
-  res.setHeader('Content-Type', 'text/plain');
-
-  let url = req.url; //i.e localhost:3000/contact
-
-  switch (url) {
-    case "/":
-      stream.readFile('index.html',null, ((err, data) => {
-        if (err) {
-          res.writeHead(404);
-          res.write('404 not found');
-        }
-        else{
-          res.write(data);
-        }
-      }))
-      break;
-
-    case "/contact":
-    stream.readFile('contact.html',null, ((err, data) => {
-      if (err) {
-        res.writeHead(404);
-        res.write('404 not found');
-      }
-      else{
-        res.write(data);
-      }
-    }))
-    break;
-
-    case "/portfolio":
-    stream.readFile('portfolio.html',null, ((err, data) => {
-      if (err) {
-        res.writeHead(404);
-        res.write('404 not found');
-      }
-      else{
-        res.write(data);
-      }
-    }))
-    break;
-
-    default:
-      res.end('Ko No Dio Da!');//serve up custom error page
-
-  }
-});
 
 server.listen(port, () => {
   console.log(`Server running at ${port}/`);
